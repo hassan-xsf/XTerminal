dafdbb311183e10e38b9ee677a1692c42c8ea001
Hassan <hassanshakil4569@gmail.com>
2024-11-22 00:49:52 +0500

    chores: unused import

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import { SquareTerminal, Terminal } from "lucide-react";
+import { Terminal } from "lucide-react";
 
 export const metadata: Metadata = {
   title: "XTerminal",
